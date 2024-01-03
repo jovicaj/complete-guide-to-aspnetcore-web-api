b964df16861bd11dadf15c883b05c71723dcbf21
jovica <jovica@dwelt.net>
2024-01-03 17:22:02 +0100

    Add project files.

---
+++ my-books/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace my_books.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
